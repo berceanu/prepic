e53f31430606372e5589b2e028df3afa69ef12da
Andrei Berceanu <berceanu@users.noreply.github.com>
2019-06-25 14:05:55 +0300

    Pin required numpy version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['unyt>=2.2.0', ]
+requirements = ['unyt>=2.2.0', 'numpy>=1.16.4']
 
 setup_requirements = ['pytest-runner', ]
 
