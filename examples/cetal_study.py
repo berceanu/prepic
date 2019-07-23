5ba2cd6fcfccf63ca58f524fa7c3a299eed16094
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-23 14:36:18 +0300

    Add example studies for CETAL and FLAME

---
+++ examples/cetal_study.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+
+import unyt as u
+
+from prepic import lwfa
+
+Cetal = namedtuple("Cetal", ["w0", "ɛL", "τL"])
+
+if __name__ == "__main__":
+    param = Cetal(w0=18.0 * u.micrometer, ɛL=7.7 * u.joule, τL=40.0 * u.femtosecond)
+
+    beam = lwfa.GaussianBeam.from_focal_distance(
+        focal_distance=3.2 * u.meter, beam_diameter=200 * u.mm
+    )
+    print(f"Smallest possible beam waist for this setup is w0={beam.w0:.1f}.\n")
+
+    laser = lwfa.Laser(ɛL=param.ɛL, τL=param.τL, beam=lwfa.GaussianBeam(w0=param.w0))
+    print(f"{laser}\n")
+
+    matched_plasma = lwfa.matched_laser_plasma(a0=laser.a0)
+    print(f"{matched_plasma}\n")
+
+    print(f"Plasma density: {matched_plasma.npe:.1e}")
+    print(f"Acceleration distance: {matched_plasma.Lacc:.1f}")
+    print(f"Electron energy gain: {matched_plasma.ΔE:.1f}")
+    print(f"Total accelerated charge: {matched_plasma.Q:.1f}")
---
+++ examples/flame_study.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+
+import unyt as u
+
+from prepic import lwfa
+
+Flame = namedtuple("Flame", ["npe", "w0", "ɛL", "τL", "prop_dist", "f_dist", "diam"])
+
+if __name__ == "__main__":
+    # we take the pulse energy to be the energy in focus, i.e. 40% of 3J.
+    param = Flame(
+        npe=1.0e18 / u.cm ** 3,
+        w0=15.0 * u.micrometer,
+        ɛL=1.2 * u.joule,
+        τL=28.0 * u.femtosecond,
+        prop_dist=5.0 * u.mm,
+        f_dist=1.0 * u.meter,
+        diam=100 * u.mm,
+    )
+
+    beam = lwfa.GaussianBeam.from_focal_distance(
+        focal_distance=param.f_dist, beam_diameter=param.diam
+    )
+    print(
+        (
+            f"The smallest possible beam waist for an OAP with focal lenght of {param.f_dist:.1f} "
+            f"and a laser beam diameter of {param.diam:.1f} is w0={beam.w0:.1f} at 1/e^2 in intensity.\n"
+        )
+    )
+
+    laser = lwfa.Laser(ɛL=param.ɛL, τL=param.τL, beam=lwfa.GaussianBeam(w0=param.w0))
+
+    plasma = lwfa.Plasma(
+        n_pe=param.npe,
+        laser=laser,
+        bubble_radius=param.w0,
+        propagation_distance=param.prop_dist,
+    )
+
+    print(
+        (
+            f"For a plasma with a density of {plasma.npe:.1e} we get an "
+            f"electron energy gain of {plasma.ΔE:.1f} and a total accelerated charge "
+            f"of {plasma.Q:.1f} over an acceleration distance of {plasma.Lacc:.1f}."
+        )
+    )
+
+    print("\nFurther details: ")
+    print(f"{plasma}")
