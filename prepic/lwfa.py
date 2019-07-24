3a5dd40409b44f968d2452ac2ce833d2d9e9ee6a
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-24 15:29:46 +0300

    Add radiation-reaction importance estimation

--- prepic/lwfa.py
+++ prepic/lwfa.py
@@ -402,6 +402,8 @@ class Radiator(BaseClass):
     ----------
     α: :obj:`unyt_quantity`
         Fine structure constant.
+    τ0: :obj:`unyt_quantity`
+        Radiation-reaction time-scale.
     a: :obj:`unyt_quantity`
         Scaling constant used to calculate the betatron amplitude.
     b: :obj:`unyt_quantity`
@@ -429,6 +431,8 @@ class Radiator(BaseClass):
         Opening angle of the radiation in the plane perpendicular to electron oscillations.
     N_shot: :obj:`unyt_quantity`
         Total number of radiated photons per laser shot.
+    N_RR: :obj:`unyt_quantity`
+        Threshold number of oscillations for noticeable radiation-reaction effects.
 
     Note
     ----
@@ -440,6 +444,7 @@ class Radiator(BaseClass):
     """
 
     α = (u.qe ** 2 / (4 * np.pi * u.eps_0 * u.hbar * u.clight)).to("dimensionless")
+    τ0 = ((2 * r_e / (3 * u.clight))).to("yoctosecond")
     a = 1510.3 * u.micrometer ** (-1 / 2)
     b = 3 * np.sqrt(2.0e19) * u.cm ** (-3 / 2)
 
@@ -465,6 +470,9 @@ class Radiator(BaseClass):
         self.λu = (np.sqrt(2 * self.γ) * self.plasma.λp).to("mm")
         self.K = (np.sqrt(self.γ / 2) * self.plasma.kp * self.rβ).to("dimensionless")
         self.θ_perp = (1 / self.γ * u.radian).to("miliradian")
+        self.N_RR = (
+            self.λu / (2 * np.pi ** 2 * u.clight * self.τ0 * self.γ * self.K ** 2)
+        ).to("dimensionless")
 
         if self.K > 5:  # wiggler case
             self.ħωc = (3 / 2 * self.K * self.γ ** 2 * u.h * u.clight / self.λu).to(
@@ -485,8 +493,12 @@ class Radiator(BaseClass):
             f"and per electron is Nᵧ = {self.Nγ.to_value('dimensionless'):.1f}, while the total number of "
             f"betatron photons per laser shot is {self.N_shot.to_value('dimensionless'):.1e}. "
             f"The half width of the angular distribution of the radiated energy in the "
-            f"electron oscillation plane is θᵣ = {self.θ_par:.1f}."
+            f"electron oscillation plane is θᵣ = {self.θ_par:.1f}. "
         )
+        if self.N_RR > 10 * self.Nβ:
+            msg += f"Radiation-reaction effects are negligible."
+        else:
+            msg += f"Radiation-reaction effects should be taken into consideration!"
         return msg
 
     def __repr__(self):
