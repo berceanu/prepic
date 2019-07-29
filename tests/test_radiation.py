ff285bc3a389d6afc6f74d2f73cd1e959bb5b33c
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-29 23:01:52 +0300

    Add Radiation unit-test

--- tests/test_radiation.py
+++ tests/test_radiation.py
@@ -2,3 +2,14 @@
 Tests for prepic.radiation module
 
 """
+import pytest
+
+from prepic import Radiator, Plasma
+
+
+def test_radiator_without_laser(cet_param):
+    """Check Radiator class constructor without passing a Laser."""
+    plasma = Plasma(n_pe=cet_param.npe, bubble_radius=cet_param.w0)
+
+    with pytest.raises(TypeError):
+        _ = Radiator(plasma)
