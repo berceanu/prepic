d29ce918ad2bcaef6dc5389f6bdb01b8bbe329bd
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-31 00:41:15 +0300

    Add new example using spectrum classes

---
+++ examples/new_sync.py
@@ -0,0 +1,26 @@
+import unyt as u
+from collections import namedtuple
+from prepic import GaussianBeam, Laser, Plasma, Radiator
+
+
+Param = namedtuple("Param", ["npe", "w0", "ɛL", "τL", "prop_dist"])
+
+p = Param(  # external guiding / injection example from from http://doi.org/f4j98s
+    npe=5.1e17 / u.cm ** 3,
+    w0=21.0 * u.micrometer,
+    ɛL=3.0 * u.joule,
+    τL=47.0 * u.femtosecond,
+    prop_dist=52.0 * u.mm,
+)
+
+laser = Laser(ɛL=p.ɛL, τL=p.τL, beam=GaussianBeam(w0=p.w0))
+
+plasma = Plasma(
+    n_pe=p.npe, laser=laser, bubble_radius=p.w0, propagation_distance=p.prop_dist
+)
+
+radiator = Radiator(plasma=plasma)
+
+canvas = radiator.frequency_spectrum().plot()
+
+canvas.savefig(f"caca.png")
