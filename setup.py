930566c807529246cf81889ef222b62cba286d1e
Andrei Berceanu <andreicberceanu@gmail.com>
2019-10-02 12:26:53 +0200

    Add new dependencies and pin all of them

--- setup.py
+++ setup.py
@@ -11,7 +11,14 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["unyt>=2.3.1", "numpy>=1.16.4", "scipy>=1.3.0", "matplotlib>=3.1.1"]
+requirements = [
+    "unyt==2.3.1",
+    "numpy==1.17.2",
+    "scipy==1.3.1",
+    "matplotlib==3.1.1",
+    "matplotlib-label-lines==0.3.6",
+    "sliceplots==0.3.1",
+]
 
 setup_requirements = ["pytest-runner"]
 
